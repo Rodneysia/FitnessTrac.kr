1ebdc8e1b0ba27f3ccc22328a94babc608f834f9
root <root@Michael-PC>
2023-02-15 20:24:00 -0600

    delete robot

---
+++ my-app/src/api/index.js
@@ -0,0 +1,35 @@
+const URL = `https://fitnesstrac-kr.herokuapp.com/api/`
+
+export async function getActivities(){
+    try{
+        const response = await fetch(`${URL}activities`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer TOKEN_STRING_HERE'
+            },
+            body: JSON.stringify({ /* whatever things you need to send to the API */ })
+            return response;
+          })
+    }
+    catch(err){
+        console.error('error', err);
+    }
+}
+
+export async function getRoutines(){
+    try{
+        const response = await fetch(`${URL}routines`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer TOKEN_STRING_HERE'
+  },
+  body: JSON.stringify({ /* whatever things you need to send to the API */ })
+  return response;
+    })}
+    catch(err){
+        console.error('error', err);
+    }
+}
+
